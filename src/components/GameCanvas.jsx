2934b5efb561ebd031f3ec418ed9edcf456af301
gabriel <gabriel14em@gamil.com>
2025-07-04 20:40:45 -0300

    terminei de copiar a base do codigo

--- src/components/GameCanvas.jsx
+++ src/components/GameCanvas.jsx
@@ -14,4 +14,22 @@ const GameCanvas = () => {
     )
 }
 
+const [position, setPosition] = useState({ x: 100, y: 300});
+
+useEffect(() => {
+    const handleKeyDown = (e) => {
+        setPosition(pos => {
+            switch (e.key) {
+                case 'ArrowRight': return {...pos, x: pos.x + 5}; //move pra direita
+                case 'ArrowLeft': return {...pos, x: pos.x - 5}; //move pra esquerda
+                case 'ArrowUp': return {...pos, y: pos.y - 5}; //move pra cima
+                case 'ArrowDown': return {...pos, y: pos.y + 5}; //move pra baixo
+                default: return pos;
+            }
+        });
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+})
+
 export default GameCanvas;
